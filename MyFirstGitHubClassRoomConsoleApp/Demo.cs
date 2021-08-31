e0769fd28d5b9f21a61abb8270da0587493b5df8
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2021-08-31 14:24:21 +0200

    changes

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        int _property1;
+        string _property2;
+        public Demo(int p1, string p2)
+        {
+            _property1 = p1;
+            _property2 = p2;
+        }
+        public int Property1
+        {
+            get
+            {
+                return _property1;
+            }
+            set
+            {
+                _property1 = value;
+            }
+        }
+        public string Property2
+        {
+            get
+            {
+                return _property2;
+            }
+            set
+            {
+                _property2 = value;
+            }
+        }
+    }
+}
